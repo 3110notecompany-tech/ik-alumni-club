5ab5c96d80d5fc6ece8a59c7b1bec91a04f77975
3110notecompany-tech <3110note.company@gmail.com>
2025-10-01 19:16:13 +0900

    fix:ログインフォームを追加

---
+++ components/login-form/login-form.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+import coverImage from "./login-form-cover.jpg"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">ようこそ</h1>
+                <p className="text-muted-foreground text-balance">
+                  IK同窓会クラブにログイン
+                </p>
+              </div>
+              <Button type="submit" className="w-full">
+                ゲストでログイン
+              </Button>
+            </div>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <Image
+              src={coverImage}
+              alt=""
+              placeholder="blur"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
