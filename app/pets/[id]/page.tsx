a96cff9920a158f0c5972069329f5bc4f81d6639
3110notecompany-tech <3110note.company@gmail.com>
2025-10-03 16:10:39 +0900

    fix:削除機能追加

--- app/pets/[id]/page.tsx
+++ app/pets/[id]/page.tsx
@@ -1,20 +1,23 @@
+import { DeletePetButton } from "@/components/delete-pet-botton";
 import { PetCard } from "@/components/pet-card";
 import { PetForm } from "@/components/pet-form";
 import { getPet } from "@/data/pet";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function PetPage({ params }: PageProps<"/pets/[id]">) {
   const petId = (await params).id;
   const pet = await getPet(petId);
 
   if (!pet) {
-    notFound();
+    redirect("/pets");
   }
   return (
     <div className="container">
       <PetCard pet={pet} />
 
       <PetForm defaultValues={pet} />
+
+      <DeletePetButton petId={pet.id} />
     </div>
   );
 }
