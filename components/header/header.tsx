fbe92550cdba722c2a45c4dab58a35668f6a8c6c
3110notecompany-tech <3110note.company@gmail.com>
2025-10-07 05:09:46 +0900

    fix:ヘッダー作成

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,6 +3,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "./logo.png";
+import divider from "./line.svg";
+import xBlack from "../sns-icon/x-black.png";
+import instagramBlack from "../sns-icon/instagram-black.png";
+import youtubeBlack from "../sns-icon/youtube-black.png";
+import tiktokBlack from "../sns-icon/tiktok-black.png";
+
 import { Button } from "@/components/ui/button";
 import { Menu, User, LogOut, Dog } from "lucide-react";
 import {
@@ -13,9 +19,11 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 export function Header() {
   const router = useRouter();
+  const t = useTranslations("Header");
 
   // ユーザー登録機能実装時に使用
   // const { data: session } = authClient.useSession();
@@ -28,7 +36,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-[140px] items-center justify-between">
+      <div className="container max-w-full flex h-[140px] items-center justify-between">
         {/* ロゴ */}
         <Image
           src={logo}
@@ -38,7 +46,26 @@ export function Header() {
           placeholder="blur"
           className="dark:brightness-[0.2] dark:grayscale"
         />
-        <div className="header-text">TEXT</div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-6 h-[70px] items-center justify-end">
+            <div className="header-text">{t("home")}</div>
+            <div className="header-text">{t("information")}</div>
+            <div className="header-text">{t("schedule")}</div>
+            <div className="header-text">{t("video")}</div>
+            <div className="header-text">{t("blog")}</div>
+            <div className="header-text">{t("newsletters")}</div>
+            <div className="header-text">{t("contact")}</div>
+            <Image src={divider} alt="" width={1} height={18} />
+            <Image src={xBlack} alt="" height={24} />
+            <Image src={instagramBlack} alt="" height={24} />
+            <Image src={youtubeBlack} alt="" height={24} />
+            <Image src={tiktokBlack} alt="" height={24} />
+          </div>
+          <div className="flex gap-6 h-[70px] items-center justify-end">
+            <Button className="rounded-[50px] bg-brand">{t("joinUs")}</Button>
+            <Button variant="outline" className="rounded-[50px] border-brand">{t("login")}</Button>
+          </div>
+        </div>
         {/* マイページボタンとログアウト//ユーザー登録機能実装時に使用 */}
         {/* <div className="flex items-center gap-2">
           {session?.user && (
