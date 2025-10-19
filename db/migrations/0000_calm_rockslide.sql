c407fc4b12c2da862d81ca5642f0de380b6c3b1b
3110notecompany-tech <3110note.company@gmail.com>
2025-10-19 22:42:25 +0900

    fixマイグレーションの実行エラーの修正

--- db/migrations/0000_calm_rockslide.sql
+++ db/migrations/0000_calm_rockslide.sql
@@ -1,4 +1,3 @@
-CREATE TYPE "public"."pet_type" AS ENUM('dog', 'cat');--> statement-breakpoint
 CREATE TABLE "accounts" (
 	"id" text PRIMARY KEY NOT NULL,
 	"account_id" text NOT NULL,
@@ -48,14 +47,5 @@ CREATE TABLE "verifications" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "pets" (
-	"id" text PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"type" "pet_type" NOT NULL,
-	"hp" integer DEFAULT 50 NOT NULL,
-	"owner_id" text NOT NULL
-);
---> statement-breakpoint
 ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "pets" ADD CONSTRAINT "pets_owner_id_users_id_fk" FOREIGN KEY ("owner_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
