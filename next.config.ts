676674b695c1b47abc6e2ba65f7e10881a931e39
3110notecompany-tech <3110note.company@gmail.com>
2025-10-22 00:13:03 +0900

    fix:画像のURLを全て許可するように変更

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'www.kashiwa-cci.or.jp',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
