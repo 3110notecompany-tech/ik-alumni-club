c4f7c412e07236635809a2c804a9f6b7066a7720
3110notecompany-tech <3110note.company@gmail.com>
2025-11-17 23:52:23 +0900

    fix:メイン画像の変更

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -18,12 +18,11 @@ export default async function Home({
 
   return (
     <div className="font-sans max-w-full">
-      <div className="relative w-full h-[50vh] sm:h-[70vh] md:h-screen -mt-20 md:-mt-35 mb-16 md:mb-32">
+      <div className="relative w-full mb-16 md:mb-32">
         <Image
           src={heroBg}
           alt="Hero Background"
-          fill
-          className="object-cover"
+          className="w-full h-auto"
           priority
         />
       </div>
